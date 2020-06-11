99d945d4f178653706b774b8166c77fb951a8711
austin362667 <austin362667@gmail.com>
2020-06-11 22:42:31 +0800

    add code

--- server/controller.ts
+++ server/controller.ts
@@ -51,7 +51,7 @@ class Controller {
       // await Deno.writeFile(`${Deno.cwd()}/public/file/img/${fsn}.jpeg`, data);
       console.log(context.uploadedFiles);
       context.response.headers.set("Content-Type", "application/json");
-      context.response.body = { "data": url };
+      context.response.body = { "data": uri };
     } catch (e) {
       console.log(`UserController.upload=>${e}`);
     }
