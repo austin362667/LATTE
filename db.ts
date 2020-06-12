f81c9fc5eb6e7b33e1a39acf947c286ee7bf4704
austin362667 <austin362667@gmail.com>
2020-06-12 14:25:44 +0800

    add code

--- db.ts
+++ db.ts
@@ -2,7 +2,8 @@ import { postgre } from "./lib.ts";
 const Client = postgre.Client;
 
 const Db = new Client({
-  user: "Austin",
+  user: "postgres",
+  password: "latte-a1",
   database: "db",
   hostname: "localhost",
   port: 5432,
