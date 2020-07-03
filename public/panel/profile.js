ceceb98df4bc0dfeb014af27ebfaed7ae045c241
austin362667 <austin362667@gmail.com>
2020-07-03 15:02:44 +0800

    add chat

--- public/panel/profile.js
+++ public/panel/profile.js
@@ -2,10 +2,10 @@ const profile = {};
 
 profile.start = async function () {
   var userDb = {};
-  var { data } = {};
+  var data = {};
   const r = await tool.postJson("/api/v1.0/user/profile");
   r.ok ? { data } = await r.json() : {};
-  r.ok ? userDb = data : userDb = {};
+  userDb = r.ok ? data : {};
   r.ok
     ? tool.show(`<h3 class="info">Name: ${userDb.name}<br>Email: ${userDb.email}</h3>`)
     : tool.show(`<p>You must login to view your profile!</p>`);
