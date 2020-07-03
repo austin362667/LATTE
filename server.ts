86da6c1c50afbc515e9d2a361676a9b664cf87ff
austin362667 <austin362667@gmail.com>
2020-07-04 01:14:06 +0800

    fix ws ssl

--- server.ts
+++ server.ts
@@ -95,31 +95,93 @@ app.addEventListener("error", (evt) => {
 
 const main = async function () {
 
-// websocket serve
-const socketServer = new SocketServer();
-socketServer.run(optionsWS)
-socketServer.on("connection", function (ws: any) {
+// // websocket serve
+// const socketServer = new SocketServer();
+// socketServer.run(optionsWS)
+// socketServer.on("connection", function (ws: any) {
 
-	ws.on("message", function (message: string) {
+// 	ws.on("message", function (message: string) {
 
-		console.log(message);
-		//ws.send(message);
+// 		console.log(message);
+// 		//ws.send(message);
 
-		// broadcast message
-		socketServer.clients.forEach(function each(client:any) {
-			if (!client.isClosed) {
-				client.send(message);
-			}
-		});
+// 		// broadcast message
+// 		socketServer.clients.forEach(function each(client:any) {
+// 			if (!client.isClosed) {
+// 				client.send(message);
+// 			}
+// 		});
 
-	});
+// 	});
 
-});
+// });
 
 
   console.log("Server Up!");
   console.log(__dirname)
   app.listen(options);
 };
+
+
+
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+import { serve } from "https://deno.land/std/http/server.ts";
+import {
+  acceptWebSocket,
+  isWebSocketCloseEvent,
+  isWebSocketPingEvent,
+  WebSocket,
+} from "https://deno.land/std/ws/mod.ts";
+
+async function handleWs(sock: WebSocket) {
+  console.log("socket connected!");
+  try {
+    for await (const ev of sock) {
+      if (typeof ev === "string") {
+        // text message
+        console.log("ws:Text", ev);
+        await sock.send(ev);
+      } else if (ev instanceof Uint8Array) {
+        // binary message
+        console.log("ws:Binary", ev);
+      } else if (isWebSocketPingEvent(ev)) {
+        const [, body] = ev;
+        // ping
+        console.log("ws:Ping", body);
+      } else if (isWebSocketCloseEvent(ev)) {
+        // close
+        const { code, reason } = ev;
+        console.log("ws:Close", code, reason);
+      }
+    }
+  } catch (err) {
+    console.error(`failed to receive frame: ${err}`);
+
+    if (!sock.isClosed) {
+      await sock.close(1000).catch(console.error);
+    }
+  }
+}
+
+if (import.meta.main) {
+  /** websocket echo server */
+  const port = Deno.args[0] || "8080";
+  console.log(`websocket server is running on :${port}`);
+  for await (const req of serve(`:${port}`)) {
+    const { conn, r: bufReader, w: bufWriter, headers } = req;
+    acceptWebSocket({
+      conn,
+      bufReader,
+      bufWriter,
+      headers,
+    })
+      .then(handleWs)
+      .catch(async (err) => {
+        console.error(`failed to accept websocket: ${err}`);
+        await req.respond({ status: 400 });
+      });
+  }
+}
+
 
 main();
