949b5402cb7d2cd58379ec4b6549a36890227a16
austin362667 <austin362667@gmail.com>
2020-08-17 00:41:31 +0800

    add ssl

--- server.ts
+++ server.ts
@@ -5,11 +5,11 @@ import { WebSocket, WebSocketServer } from "https://deno.land/x/websocket/mod.ts
 
 
 const options = {
-  // secure: true,
+  secure: true,
   // port: 443,
   port: 80,
-  // certFile: "/etc/letsencrypt/live/lattemall.company/fullchain.pem",
-  // keyFile: "/etc/letsencrypt/live/lattemall.company/privkey.pem",
+  certFile: "/etc/letsencrypt/live/lattemall.company/fullchain.pem",
+  keyFile: "/etc/letsencrypt/live/lattemall.company/privkey.pem",
 };
 
 
