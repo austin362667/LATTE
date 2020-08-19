138abca89ea12719645caafdf4e7806d277496f7
austin362667 <austin362667@gmail.com>
2020-08-19 15:16:11 +0800

    fix redirect

--- server.ts
+++ server.ts
@@ -32,6 +32,14 @@ router
   // .post("/api/v1.0/post/list/id", Ctr.listById)
   .get('/chat', chatView)
   // .get('/ws', handleSocket);
+  // set up a route to redirect http to https
+  .get('*', function(ctx) {  
+  
+    ctx.redirect('https://' + ctx.request.headers.host + ctx.request.url);
+
+  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+  // res.redirect('https://example.com' + req.url);
+})
 
 
 const __dirname = new URL('.', import.meta.url).pathname;
